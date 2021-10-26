332bb8d166253902298bd72997a0b87b42d3a118
henriquenassary <henriquenassary@gmail.com>
2021-10-26 16:56:39 -0300

    Alterações scroll

--- js/modules/anima-sections.js
+++ js/modules/anima-sections.js
@@ -1,13 +1,36 @@
-export default function initanimacaoSections() {
-  const sections = document.querySelectorAll('[data-anima="scroll"]');
-  
-  if(sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
-    
-    function animaScroll() {
-      sections.forEach((i) => {
+import debounce from "./debounce.js";
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
+  }
+
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo')
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
+      }
+    })
+  }
+   
+    animaScroll() {
+      this.sections.forEach((i) => {
         const sectionTop = i.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        const isSectionVisible = (sectionTop - this.windowMetade) < 0;
         if(isSectionVisible) {
           i.classList.add('ativo');
         }
@@ -16,11 +39,17 @@ export default function initanimacaoSections() {
         }
       });
     }
-    
-    animaScroll();
-    
-    window.addEventListener('scroll', animaScroll);
-  
-  }
 
+    init() {
+      if (this.sections.length) {
+        this.getDistance();
+        this.checkDistance();
+        window.addEventListener('scroll', this.checkDistance);
+      }
+      return this;
+    }
+
+    stopAnimation() {
+      window.removeEventListener('scroll', this.checkDistance);
+    }
 }
--- js/scprit.js
+++ js/scprit.js
@@ -3,11 +3,11 @@ import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabNav.js';
 import Modal from './modules/modal.js'
 import Tooltip from './modules/tooltip.js'
-import initDropDownMenu from './modules/dropdown-menu.js'
-import initMenuMobile from './modules/menu-mobile.js'
+import MenuMobile from './modules/menu-mobile.js'
 import fetchAnimais from './modules/fetch-animais.js'
-import initFuncionamento from './modules/hora-funcionamento.js'
-import initAnimaSections from './modules/anima-sections.js';
+import Funcionamento from './modules/hora-funcionamento.js'
+import ScrollAnima from './modules/anima-sections.js';
+import DropDownMenu from './modules/dropdown-menu.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -24,17 +24,24 @@ modal.init();
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
-initDropDownMenu();
-initMenuMobile();
-initFuncionamento();
-initAnimaSections();
+const scrollAnima = new ScrollAnima('[data-anima="scroll"]');
+scrollAnima.init();
 
-fetchAnimais('https://henriquenassary.github.io/origamid/json/animaisapi.json', '.numeros-grid');
+const dropDownMenu = new DropDownMenu('[data-dropdown]');
+dropDownMenu.init();
+
+const menuMobile = new MenuMobile('[data-menu=button]','[data-menu=list]');
+menuMobile.init();
+
+const funcionamento = new Funcionamento('[data-semana]', 'aberto');
+funcionamento.init();
+
+fetchAnimais('./json/animaisapi.json', '.numeros-grid');
 
 const btnGeraNumero = document.querySelector('[data-gera="numero"]');
 const mostraNumeroAleatorio = document.querySelector('.numero-aleatorio');
 btnGeraNumero.addEventListener('click', () => {
   let min = Math.ceil(1);
-  let max = Math.floor(200);
+  let max = Math.floor(201);
   mostraNumeroAleatorio.innerText = Math.floor(Math.random() * (max - min) + min);
 });
